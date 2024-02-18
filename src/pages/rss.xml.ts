6f3d130524c172675fed98f735d8252df72f62ff
YumNumm <onory0917@gmail.com>
2024-02-18 23:34:41 +0900

    remove unused files

--- src/pages/rss.xml.ts
+++
@@ -1,9 +0,0 @@
-import rss from "@astrojs/rss";
-
-export const get = () =>
-  rss({
-    title: "Astro Basic Blog Template",
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-    site: import.meta.env.SITE,
-    items: import.meta.glob("./posts/**.mdx"),
-  });
